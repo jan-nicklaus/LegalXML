49be04ea7ed1f023d2e5f814d6ec7c534f68e8bf
Jan Reinhardt <jannicklaus33@googlemail.com>
2026-04-25 20:32:19 +0200

    Basic Rendering.

--- XML-Viewer/src/App.svelte
+++ XML-Viewer/src/App.svelte
@@ -32,8 +32,11 @@
         <Eroeffnung></Eroeffnung>
         <Begruendung>
             <Prozessuales>
-                <Section title="Test-Titel" type="first-instance">
+                <Section title="Kantonsgericht" type="first-instance">
                    <Absatz>Das Kantonsgericht hat die Klage <b>gutgeheissen</b>.</Absatz> 
+                </Section>
+                <Section title="Obergericht" type="second-instance">
+                   <Absatz>Das Obergericht ist auf die Beschwerde nicht eingetreten.</Absatz> 
                 </Section>
             </Prozessuales>
             <Formelles>
@@ -50,38 +53,45 @@
     </Main>
 </xml>`;
 
-let parser = new DOMParser();
-let xmlTree = parser.parseFromString(testXML, "text/xml");
-let docType = xmlTree.getElementById("doctype").textContent.trim();
-let xmlMain = xmlTree.getElementsByTagName("Main")[0]; //Main muss existieren
-
-function getBGColor(sec) {
-  if(!sec.hasAttribute("type")) return "slate-200";
-  switch(sec.getAttribute("type")) {
-    case "first-instance": return "black"
-  }
-}
+  let parser = new DOMParser();
+  let xmlTree = parser.parseFromString(testXML, "text/xml");
+  let docType = xmlTree.getElementById("doctype").textContent.trim();
+  let xmlMain = xmlTree.getElementsByTagName("Main")[0]; //Main muss existieren
 </script>
 <div class="prose prose-lg m-4 w-full">
     {#if docType === "Judgment"}
     <h2>Dispositiv</h2>
 
     <h2>Begründung</h2>
-    <h3>Prozessuales</h3>
-    {#each xmlMain.getElementsByTagName("Prozessuales")[0].getElementsByTagName("Section") as sec}
-      <div class={`mt-4 relative rounded-md bg-${getBGColor(sec)}`}>
-        {#if sec.hasAttribute("type")}
-          <div class="absolute top-2 right-2 badge badge-accent">
-            {sec.getAttribute("type")}
-          </div>
-        {/if}
-        {#if sec.hasAttribute("title")}
-          <h4>{sec.getAttribute("title")}</h4>
-        {/if}
-        {#each sec.getElementsByTagName("Absatz") as par}
-          <div>{@html par.innerHTML}</div>
-        {/each}
-      </div>
+    {#each ["Prozessuales", "Formelles", "Materielles"] as h}
+      <details class="collapse bg-base-100 border border-base-300 mt-4" name={`accordion-${h}`} open>
+        <summary class="collapse-title font-semibold">{h}</summary>
+        <div class="collapse-content">
+          {#if xmlMain.getElementsByTagName(h)[0].getElementsByTagName("Section").length < 2}
+            {#each xmlMain.getElementsByTagName(h)[0].getElementsByTagName("Section")[0].getElementsByTagName("Absatz") as par}
+              <div>{@html par.innerHTML}</div>
+            {/each}
+          {:else}
+            {#each xmlMain.getElementsByTagName(h)[0].getElementsByTagName("Section") as sec, i}
+            <details class="collapse bg-base-100 border border-base-300 mt-4" name={`accordion-${h}-${i}`} open>
+              <summary class="collapse-title font-semibold">
+                {sec.hasAttribute("title") ? sec.getAttribute("title") : "Sektion"}
+                {#if sec.hasAttribute("type")}
+                  <div class="absolute top-2 right-2 badge badge-accent">
+                    {sec.getAttribute("type")}
+                  </div>
+                {/if}
+              </summary>
+              <div class="collapse-content">
+                {#each sec.getElementsByTagName("Absatz") as par}
+                  <div>{@html par.innerHTML}</div>
+                {/each}
+                </div>
+            </details>
+            {/each}
+          {/if}
+        </div>
+      </details>
     {/each}
   {/if}
 </div>
