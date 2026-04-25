c336e5f858f84d0508de0c6409ea11445adc207a
Jan Reinhardt <jannicklaus33@googlemail.com>
2026-04-25 17:53:58 +0200

    DOM Structure.

--- XML-Viewer/src/App.svelte
+++ XML-Viewer/src/App.svelte
@@ -1,6 +1,60 @@
 <script>
+  let testXML = `<xml>
+    <Rubrum>
+        <Aktenzeichen></Aktenzeichen>
+        <EntscheidTyp>Urteil</EntscheidTyp>
+        <EntscheidDatum></EntscheidDatum>
+        <Spruchkoerper>
+            <Gericht></Gericht>
+            <Kammer></Kammer>
+            <Zusammensetzung>
+                <Mitglied role="judge">Mustermann</Mitglied>
+            </Zusammensetzung>
+        </Spruchkoerper>
+        <Parteien>
+            <Partei>
+                <Rollen>
+                    <Rolle>Gesuchsteller</Rolle>
+                    <Rolle>Beschwerdeführer</Rolle>
+                </Rollen>
+                <Name></Name>
+                <Anschrift></Anschrift>
+                <Stellvertretung></Stellvertretung>
+                <Rechtsvertretung></Rechtsvertretung>
+            </Partei>
+        </Parteien>
+    </Rubrum>
+    <Main>
+        <Dispositiv>
 
+        </Dispositiv>
+        <Eroeffnung></Eroeffnung>
+        <Begruendung>
+            <Prozessuales>
+                <Section>
+                   <Absatz></Absatz> 
+                </Section>
+            </Prozessuales>
+            <Formelles>
+                <Section>
+                   <Absatz></Absatz> 
+                </Section>
+            </Formelles>
+            <Materielles>
+                <Section type="ruege">
+                    <Absatz>Die Klägerin führt aus</Absatz>
+                </Section>
+            </Materielles>
+        </Begruendung>
+    </Main>
+</xml>`;
+
+let parser = new DOMParser();
+let xmlTree = parser.parseFromString(testXML, "text/xml");
 </script>
+
+
+
 
 <h1 class="text-red-400">Test</h1>
 <button class="btn">Test</button>
